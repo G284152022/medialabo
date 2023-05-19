80c5b58451842155659d36bac3726ac15068c959
G284152022 <228415@st.takushoku-u.ac.jp>
2023-05-20 01:23:20 +0900

    作成

--- kazuate.js
+++ kazuate.js
@@ -9,32 +9,47 @@ let a = 1
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 hantei();
-hantei();
-hantei();
-hantei();
+
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+
+  let p =document.querySelector('span#kaisu');
+  p.textContent = a; 
+
+  let p1 =document.querySelector('span#answer');
+  p1.textContent = yoso; 
+
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-  console.log(a+'回目の予想:'+yoso);
+
+  //console.log(a+'回目の予想:'+yoso);
+
+  let ps2 =document.querySelector('p#result');
   if(a<4){
+    
     if(kotae===yoso){
-      console.log('正解です.おめでとう！');
+      ps2.textContent='正解です.おめでとう！';
       a=a+4;
     }else if(kotae>yoso){
-      console.log('まちがい.答えはもっと大きいですよ');
+      ps2.textContent='まちがい.答えはもっと大きいですよ';
       a=a+1;
     }else{
-      console.log('まちがい.答えはもっと小さいですよ');
+      ps2.textContent='まちがい.答えはもっと小さいですよ';
       a=a+1;
     }
   }else{
-    console.log('答えは '+kotae+'でした.すでにゲームは終わっています');
+    ps2.textContent='答えは '+kotae+'でした.すでにゲームは終わっています';
     a=a+1;
   }
-
+  
 }
+
+
+
+
+
+
